bb2d23489f19fa5c93d5b5757423aa6209e4e8da
Nikolay Nemshilov <nemshilov@gmail.com>
2013-06-09 18:46:12 +1000

    Adding the push-state feature support

--- lib/osom_tables/railtie.rb
+++ lib/osom_tables/railtie.rb
@@ -2,4 +2,12 @@ class OsomTables::Railtie < Rails::Railtie
   initializer "osom_tables.view_helpers" do
     ActionView::Base.send :include, OsomTables::Helper
   end
+
+  initializer "osom_tables.configure_rails_initialization" do
+    ActionController::Base.instance_eval do
+      before_filter do
+        params.delete(:sneaky_cache_killa)
+      end
+    end
+  end
 end
--- vendor/assets/javascripts/osom-tables.js
+++ vendor/assets/javascripts/osom-tables.js
@@ -9,17 +9,81 @@
 
   $(document).on('click', '.osom-table .pagination a', function(e) {
     e.preventDefault();
-
-    var container = $(this).closest('.osom-table');
-    var push      = container.data('push');
-    var url       = container.data('url');
-
-    container.addClass('loading');
-
-    $.get(this.getAttribute('href'), function(result) {
-      container.html(result);
-      container.removeClass('loading');
-    });
+    load_table($(this).closest('.osom-table'), this.getAttribute('href'));
   });
+
+  $(window).on('popstate', function(e) {
+    var state = e.originalEvent.state;
+    if (state && state.url) {
+      if (current_table && current_table.find('table').data('push')) {
+        load_table(current_table, state.url, true);
+      } else {
+        document.location.href = state.url;
+      }
+    }
+  });
+
+  var current_table = null;
+
+  function load_table(container, url, no_push) {
+    current_table = container.addClass('loading');
+
+    if (history.pushState && !no_push && container.find('table').data('push')) {
+      history.pushState({url: url}, 'osom-table', url);
+    }
+
+    $.ajax(build_url(url, {sneaky_cache_killa: true}), {
+      success: function(new_content) {
+        container.html(new_content);
+      },
+      complete: function() {
+        container.removeClass('loading');
+      }
+    });
+  };
+
+  /**
+   * Rebuilds the url with the extra prams
+   */
+  function build_url(url, params) {
+    var path, args; path = parse_url(url);
+    args = path[1]; path = path[0];
+
+    for (var key in params) {
+      args[key] = params[key];
+    }
+
+    return path + "?" + $.param(args);
+  }
+
+  /**
+   * Parsing the arguments out of the url query
+   */
+  function parse_url(url) {
+    var path, query, args={}, list, key, value;
+    path = url.split("?"); query = path[1]; path = path[0];
+
+    if (query) {
+      for (var i=0, list = query.split('&'); i < list.length; i++) {
+        key   = list[i].split('=');
+        value = key[1]; key = key[0];
+
+        key   = decodeURIComponent(key);
+        value = decodeURIComponent((value||'').replace(/\+/g, ' '));
+
+        if (key.substr(-2) === "[]") {
+          if (args[key]) {
+            args[key].push(value)
+          } else {
+            args[key] = [value]
+          }
+        } else {
+          args[key] = value
+        }
+      }
+    }
+
+    return [path, args];
+  }
 
 })(jQuery);
