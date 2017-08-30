8f2c75196cf0da49ba80621d7f778b7b88c30920
Huy Le <lvthuy@gmail.com>
2017-08-30 08:48:27 +0700

    Update before_filter to before_action to compatible with Rails 5 (#14)
    * Update before_filter to before_action to compatible with Rails 5

--- lib/osom_tables/railtie.rb
+++ lib/osom_tables/railtie.rb
@@ -5,7 +5,7 @@ class OsomTables::Railtie < Rails::Railtie
 
   initializer "osom_tables.configure_rails_initialization" do
     ActionController::Base.instance_eval do
-      before_filter do
+      before_action do
         params.delete(:osom_tables_cache_killa)
       end
     end
